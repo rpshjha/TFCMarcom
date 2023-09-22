2548699673f28ec9babd5526b2018a1f3355987d
rpshjha@gmail.com <rpshjha@gmail.com>
2023-09-22 17:51:04 +0530

    few changes related to test

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,38 +1,73 @@
 package org.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.example.utils.ReadJson;
+import org.hamcrest.Matchers;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+
+    RequestSpecification requestSpecification;
+    ResponseSpecification responseSpecification = null;
+
+    public PrintStream log;
+    RequestLoggingFilter requestLoggingFilter;
+    ResponseLoggingFilter responseLoggingFilter;
+
+    @BeforeTest
+    void setup() {
+        requestSpecification = RestAssured.given();
+        requestSpecification.baseUri(ReadJson.get("base-uri"));
+        requestSpecification.param("apikey", ReadJson.get("api-key"));
+
+        try {
+            log = new PrintStream(new FileOutputStream("test_logging.txt"), true);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        requestLoggingFilter = new RequestLoggingFilter(log);
+        responseLoggingFilter = new ResponseLoggingFilter(log);
+
+        responseSpecification = RestAssured.expect();
+        responseSpecification.contentType(ContentType.JSON);
+        responseSpecification.statusCode(200);
+        responseSpecification.time(Matchers.lessThan(5000L));
+        responseSpecification.statusLine("HTTP/1.1 200 OK");
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    void searchByTitle() {
+
+        ResponseBody responseBody = RestAssured.given(requestSpecification)
+                .param("t", "titanic")
+                .get()
+                .then().spec(responseSpecification)
+                .extract().response();
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    void searchById() {
+
+        ResponseBody responseBody = RestAssured
+                .given(requestSpecification)
+                .param("i", "tt0120338")
+                .get()
+                .then().spec(responseSpecification)
+                .extract().response();
+
     }
 }
---
+++ src/test/java/org/example/utils/ReadJson.java
@@ -0,0 +1,25 @@
+package org.example.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
+
+public class ReadJson {
+
+    public static String get(String key) {
+
+        Map<String, String> objectMap;
+        try {
+            String data = new String(Objects.requireNonNull(ReadJson.class.getClassLoader().getResourceAsStream("data.json")).readAllBytes());
+            ObjectMapper mapper = new ObjectMapper();
+
+            objectMap = mapper.readValue(data, Map.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return String.valueOf(objectMap.get(key));
+    }
+}
