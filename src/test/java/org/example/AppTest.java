b720848edbf5ffadcf0f1f770a1fc1185ad51e8c
rpshjha@gmail.com <rpshjha@gmail.com>
2023-09-25 10:24:02 +0530

    log related changes

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,8 +1,6 @@
 package org.example;
 
 import io.restassured.RestAssured;
-import io.restassured.filter.log.RequestLoggingFilter;
-import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
@@ -12,10 +10,6 @@ import org.hamcrest.Matchers;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
-
 /**
  * Unit test for simple App.
  */
@@ -24,29 +18,29 @@ public class AppTest {
     RequestSpecification requestSpecification;
     ResponseSpecification responseSpecification = null;
 
-    public PrintStream log;
-    RequestLoggingFilter requestLoggingFilter;
-    ResponseLoggingFilter responseLoggingFilter;
-
     @BeforeTest
     void setup() {
-        requestSpecification = RestAssured.given();
+        requestSpecification = RestAssured.given().filter(new RestAssuredRequestFilter());
         requestSpecification.baseUri(ReadJson.get("base-uri"));
         requestSpecification.param("apikey", ReadJson.get("api-key"));
-
-        try {
-            log = new PrintStream(new FileOutputStream("test_logging.txt"), true);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        requestLoggingFilter = new RequestLoggingFilter(log);
-        responseLoggingFilter = new ResponseLoggingFilter(log);
 
         responseSpecification = RestAssured.expect();
         responseSpecification.contentType(ContentType.JSON);
         responseSpecification.statusCode(200);
         responseSpecification.time(Matchers.lessThan(5000L));
         responseSpecification.statusLine("HTTP/1.1 200 OK");
+    }
+
+    @Test
+    void searchById() {
+
+        ResponseBody responseBody = RestAssured
+                .given(requestSpecification)
+                .param("i", "tt0120338")
+                .get()
+                .then().spec(responseSpecification)
+                .extract().response();
+
     }
 
     @Test
@@ -60,11 +54,11 @@ public class AppTest {
     }
 
     @Test
-    void searchById() {
+    void searchByKeyword() {
 
         ResponseBody responseBody = RestAssured
                 .given(requestSpecification)
-                .param("i", "tt0120338")
+                .param("s", "tt0120338")
                 .get()
                 .then().spec(responseSpecification)
                 .extract().response();
